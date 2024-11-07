23fec03467db1408c82f5798dc67fc3493dee250
amira abdulleiid <amira.abdulleiid@elev.ga.ntig.se>
2024-11-07 21:21:02 +0100

    jghngyhg

--- 01 If-satser, loopar, datatyper/Program.cs
+++ 01 If-satser, loopar, datatyper/Program.cs
@@ -1,16 +1,21 @@
-﻿if (6  >= 3)
+﻿
+if (6  >= 3)
 {
     Console.WriteLine("Hello, World!");
 }
 
+
 Console.WriteLine("Skriv in ditt användarnamn.");
 string username = Console.ReadLine();
 
-if (username == "kalleanka")
+Console.Write("Skriv in lösenord ");
+string password = Console.ReadLine();
+
+if (username == "kalleanka" && password == "12345")
 {
-    Console.WriteLine("Welcome");
+    Console.WriteLine("Welcome!");
 }
 else
 {
-    
+    Console.WriteLine("Wrong username or password");
 }
