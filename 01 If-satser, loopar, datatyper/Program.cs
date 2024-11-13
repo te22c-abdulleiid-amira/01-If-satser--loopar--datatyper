c5caebf763d7e74a5bb2af149cc534c14c44b4ee
amira abdulleiid <amira.abdulleiid@elev.ga.ntig.se>
2024-11-13 15:38:33 +0100

    slutövning start

--- 01 If-satser, loopar, datatyper/Program.cs
+++ 01 If-satser, loopar, datatyper/Program.cs
@@ -1,68 +1,23 @@
 ﻿
-if (6  >= 3)
-{
-    Console.WriteLine("Hello, World!");
-}
+// if (6  >= 3)
+// {
+//     Console.WriteLine("Hello, World!");
+// }
 
 
-Console.WriteLine("Skriv in ditt användarnamn.");
-string username = Console.ReadLine();
+// Console.WriteLine("Skriv in ditt användarnamn.");
+// string username = Console.ReadLine();
 
-Console.Write("Skriv in lösenord ");
-string password = Console.ReadLine();
+// Console.Write("Skriv in lösenord ");
+// string password = Console.ReadLine();
 
-if (username == "kalleanka" && password == "12345")
-    {
-        Console.WriteLine("Welcome!");
-    }
-else
-    {
-        Console.WriteLine("Wrong username or password");
-    }
+// if (username == "kalleanka" && password == "12345")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+// else
+// {
+//     Console.WriteLine("Wrong username or password");
+// }
 
-for (int i = 0; i < 32; i++)
-{
-    Console.WriteLine("Hello, World");
-}
-
-
-
-while (password != "12345")
-{
-    Console.Write("Enter password: ");
-    password = Console.ReadLine();
-
-    if (password != "12345")
-    {
-        Console.WriteLine("Wrong password, try again.");
-    }
-}
-Console.WriteLine("Access granted!");
-
-for (int i = 0; i < 5; i++)
-{
-    Console.Write("Enter a number: ");
-    int number = int.Parse(Console.ReadLine());
-
-    if (number > 5)
-    {
-        Console.WriteLine("högre än 5!");
-    }
-}
-
-
-string input;
-int result;
-
-do
-{
-    Console.Write("Enter a number: ");
-    input = Console.ReadLine();
-    
-    if (!int.TryParse(input, out result))
-    {
-        Console.WriteLine("Invalid input, please enter a valid number.");
-    }
-} while (!int.TryParse(input, out result));
-
-Console.WriteLine("You entered a valid number!");
+Random random = new Random ();
